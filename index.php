a368e5d24f612cfa6ec5b04841795e7da66d1c68
Živilė Stankevičienė <n.zivile@yahoo.com>
2018-12-14 01:28:56 +0200

    Header and Slider

---
+++ index.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" type="text/css" href="assets/css/index.css">
+</head>
+<body>
+
+<div class="header full-width">
+    <div class="header--item">
+        <a href="#" class="header--logo">
+
+        </a>
+    </div>
+    <div class="header--item">
+        <div class="header--menu">
+            <a href="#" class="header--menu-link">
+                about us
+            </a>
+            <a href="#" class="header--menu-link">
+                services
+            </a>
+            <a href="#" class="header--menu-link">
+                projects
+            </a>
+        </div>
+    </div>
+    <div class="header--item">
+        <a href="#" class="header--menu-link header--menu-link__burger">
+            menu
+        </a>
+    </div>
+</div>
+
+<div class="full-width slider">
+    <div class="slider--content">
+        <div class="slider--item">
+            prev
+        </div>
+        <div class="slider--item slider--item__center">
+            <div class="slider--title">
+                We're Charger
+            </div>
+            <div class="slider--description">
+                One of the most powerful and recognized brand names in the custom automotive industry.
+            </div>
+            <div class="slider--button">
+                read more
+            </div>
+        </div>
+        <div class="slider--item">
+            next
+        </div>
+    </div>
+</div>
+
+
+</body>
+</html>
