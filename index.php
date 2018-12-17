7132256bdf02789be93c6b31d9f4ecb13eb8a921
Živilė Stankevičienė <n.zivile@yahoo.com>
2018-12-17 18:19:23 +0200

    Slider finished.

--- index.php
+++ index.php
@@ -8,11 +8,9 @@
 
 <div class="header full-width">
     <div class="header--item">
-        <a href="#" class="header--logo">
-
-        </a>
+        <a href="#" class="header--logo"></a>
     </div>
-    <div class="header--item">
+    <div class="header--item header--item__menu">
         <div class="header--menu">
             <a href="#" class="header--menu-link">
                 about us
@@ -35,7 +33,9 @@
 <div class="full-width slider">
     <div class="slider--content">
         <div class="slider--item">
-            prev
+            <div class="slider--action slider--action__prev">
+                prev
+            </div>
         </div>
         <div class="slider--item slider--item__center">
             <div class="slider--title">
@@ -44,12 +44,48 @@
             <div class="slider--description">
                 One of the most powerful and recognized brand names in the custom automotive industry.
             </div>
-            <div class="slider--button">
-                read more
+            <div>
+                <a href="#" class="slider--button">
+                    read more
+                </a>
             </div>
         </div>
         <div class="slider--item">
-            next
+            <div class="slider--action slider--action__next">
+                next
+            </div>
+        </div>
+    </div>
+    <div class="slider--footer slider--footer__bottom">
+        <div class="slider--item">
+            <div class="slider--socials">
+                <a href="#" class="slider--social-link">
+                    Facebook
+                </a>
+                <a href="#" class="slider--social-link">
+                    Twitter
+                </a>
+                <a href="#" class="slider--social-link">
+                    Youtube
+                </a>
+            </div>
+        </div>
+        <div class="slider--item slider--item__pro">
+            <div class="slider--progress slider--progress__width">
+                <div class="slider--progress-number">
+                    01
+                </div>
+                <div class="slider--progr">
+                    <div class="slider--progress-line">
+                    </div>
+                </div>
+                <div class="slider--progress-number">
+                    10
+                </div>
+            </div>
+        </div>
+        <div class="slider--scroll">
+            Scroll down
         </div>
     </div>
 </div>
